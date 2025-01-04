f484917b3b0300505d1adcf7a4d84a776d4c822b
Dennis <dennis.diaz@epn.edu.ec>
2025-01-03 23:24:58 -0500

    NUevos Arreglos

--- src/server.js
+++ src/server.js
@@ -4,9 +4,6 @@ import morgan from 'morgan';
 import cors from 'cors';
 import RouterAdmin from './routers/RouterAdmin.js';
 import routeProveedor from './routers/RouterProveedor.js';
-import routerPaginas from './routers/RouterPaginas.js';
-import routerEstilos from './routers/RouterEstilos.js';
-import routerImagenes from './routers/RouterImagenes.js';
 
 dotenv.config()
 const app = express()
@@ -20,9 +17,6 @@ app.set('port', process.env.PORT || 3000)
 
 app.use('/api',RouterAdmin)
 app.use('/api', routeProveedor)
-app.use('/', routerPaginas)
-app.use('/Estilos', routerEstilos)
-app.use('/Imagenes', routerImagenes)
 
 app.use((req,res)=> res.status(400).send("Endpoint no encontrado"))
 
