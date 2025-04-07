9354d03b54081d1d26ec61d76eb40409567214fa
Dennis <dennis.diaz@epn.edu.ec>
2025-04-07 12:17:15 -0500

    mm

--- src/server.js
+++ src/server.js
@@ -11,16 +11,15 @@ import routerPagos from './routers/RouterPagos.js';
 dotenv.config()
 const app = express()
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://altakassa.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
     if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
+      res.header('Access-Control-Allow-Origin', 'https://altakassa.vercel.app');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, method');
+      return res.sendStatus(200)
     }
-
+  
     next();
-});
+  });
 
 app.use(express.json())
 
