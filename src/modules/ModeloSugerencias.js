8499880481ef9a93e91fd373131099bc1e7f2bbd
FAMILIA <dennisdiaz407@gmail.com>
2025-04-12 13:17:15 -0500

    Insertando el timestamp

--- src/modules/ModeloSugerencias.js
+++ src/modules/ModeloSugerencias.js
@@ -1,29 +1,31 @@
-import {Schema, model} from 'mongoose'
+import { Schema, model } from 'mongoose'
 
 const SugerenciasSchema = new Schema({
-    email:{
-        type:String,
-        require:true
+    email: {
+        type: String,
+        require: true
     },
-    rol:{
-        type:String,
-        require:true
+    rol: {
+        type: String,
+        require: true
     },
-    nombre:{
-        type:String,
-        require:true
+    nombre: {
+        type: String,
+        require: true
     },
-    experiencia:{
-        type:String,
-        require:true,
-        default:null
+    experiencia: {
+        type: String,
+        require: true,
+        default: null
     },
-    comentario:{
-        type:String,
-        require:true,
-        trim:true,
-        default:null
+    comentario: {
+        type: String,
+        require: true,
+        trim: true,
+        default: null
     }
-})
+},
+    { timestamps: true }
+)
 
 export default model('Sugerencias', SugerenciasSchema)
