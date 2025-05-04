ef0bce1672a3b2cb57c9f8bbc4cbf5bc0513912f
FAMILIA <dennisdiaz407@gmail.com>
2025-05-04 12:48:47 -0500

    Iniciando cron1

--- src/middleware/cronJobs.js
+++ src/middleware/cronJobs.js
@@ -1,5 +1,5 @@
 import cron from 'node-cron'
-import ModeloTrabajos from '../modules/ModeloTrabajos'
+import ModeloTrabajos from '../modules/ModeloTrabajos.js'
 
 
 cron.schedule('*/10 * * * *', async () =>{
