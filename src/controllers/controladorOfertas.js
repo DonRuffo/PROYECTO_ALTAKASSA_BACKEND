ebbbe993ebf05399610b54f5255e968500348318
FAMILIA <dennisdiaz407@gmail.com>
2025-05-04 22:13:02 -0500

    Ya listosds

--- src/controllers/controladorOfertas.js
+++ src/controllers/controladorOfertas.js
@@ -27,7 +27,7 @@ const crearOferta = async (req, res) => {
 
         usuario.cantidadOfertas -= 1
         await usuario.save()
-        const ofertaPop = await ModeloOfertas.findById(nuevaOferta._id).populate('nombre apellido email f_perfil monedasTrabajos')
+        const ofertaPop = await ModeloOfertas.findById(nuevaOferta._id).populate('proveedor' ,'nombre apellido email f_perfil monedasTrabajos')
         io.emit('Crear-oferta', {ofertaPop})
 
     } catch (error) {
