2808c848fa5f37e6233d961f98c52d215f9f9ca2
FAMILIA <dennisdiaz407@gmail.com>
2025-05-26 22:51:03 -0500

    socket

--- src/controllers/controladorOfertas.js
+++ src/controllers/controladorOfertas.js
@@ -30,7 +30,7 @@ const crearOferta = async (req, res) => {
         await usuario.save()
         const ofertaPop = await ModeloOfertas.findById(nuevaOferta._id).populate('proveedor', 'nombre apellido email f_perfil monedasTrabajos')
         io.emit('Crear-oferta', { ofertaPop })
-    
+        io.emit('Nueva-Oferta', { ofertaPop })
 
     } catch (error) {
         console.log(error)
