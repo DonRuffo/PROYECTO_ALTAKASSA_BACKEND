95b08c11b5192c7aa4e576993f2acb4d0930a55e
Martín Ayala <133398040+MartinXde23@users.noreply.github.com>
2025-06-04 12:48:35 -0500

    Arreglos proyect

--- src/controllers/ControladorAdmin.js
+++ src/controllers/ControladorAdmin.js
@@ -40,8 +40,8 @@ const login = async (req, res) => {
     try {
         if (Object.values(req.body).includes("")) return res.status(400).json({ msg: "Lo sentimos, debe llenar todos los campos" })
 
-        const userAd = await ModeloAdmin.findOne({ email })
-        const comparacion = await userAd.CompararContra(contrasenia);
+        const userAd = await ModuloUsuario.findOne({ email })
+        const comparacion = await userAd.CompararPasswordUsuario(contrasenia);
 
         if(comparacion) {
             const token = generarJWT(userAd._id, 'administrador');
